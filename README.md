c89f8c4662f272de7f563444f8454b10ecf5c7ff
Isak Buhl-Mortensen <isak@laiout.co>
2024-09-29 23:39:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Instructions
+
+1. Try to limit yourself to the dependencies already specified in `Cargo.toml`
+2. Modify `src/main.rs` until you get the `secret`
+3. Enter the secret in your job application form [link](todo)
+
+Don't give up, it's probably easier than you think.
+
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use reqwest::Client;
+use std::io::Write;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+
+    // NOTE: this line is just to get you started ...
+    let resp = client
+        .get("https://dev.laiout.app/applicant/getChallenge")
+        .send()
+        .await;
+
+    // follow instructions in challenge and submit a post request to
+    // applicant/checkChallengeSolution
+    //
+    //
+    // {
+    //    "instructions": <...>,
+    // }
+    //
+    // When successfull you will be given a `secret` from the `applicant/checkChallengeSolution`
+    // endpoint. Enter this secret in the job application.
+}
+
+struct Rot13Writer<T>
+where
+    T: Write,
+{
+    inner: T,
+}
+
+impl<T> Rot13Writer<T>
+where
+    T: Write,
+{
+    pub fn new(inner: T) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T> Write for Rot13Writer<T>
+where
+    T: Write,
+{
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        todo!()
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
